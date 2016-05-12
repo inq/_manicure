e0c2faaf46d2b7ee6c4969656241f2943a2b125a
inkyu <gofiri@gmail.com>
2016-05-13 05:06:56 +0900

    clean up Core.Html

--- src/Core/Html.hs
+++ src/Core/Html.hs
@@ -9,11 +9,9 @@ import qualified Data.ByteString.Char8            as BS
 import qualified Language.Haskell.TH.Quote        as TQ
 import qualified Language.Haskell.TH.Syntax       as TS
 import qualified Core.ByteString                  as ByteString
-import qualified Data.List                        as DL
 import qualified Data.ByteString.UTF8             as UTF8
 import qualified Core.Parser                      as P
-import Control.Applicative ((*>), (<*), (<$>), (<*>), (<|>))
-import Control.Monad
+import Control.Applicative ((<|>))
 
 data Attr = Dash String [Attr]
           | Attr String String
@@ -28,15 +26,16 @@ data Status = Child | Sibling | Parent
 
 instance TS.Lift Node where
     lift (Tag string attrs nodes) = [| 
-          BS.concat ([$(TS.lift $ "<" ++ string ++ print attrs ++ ">")] 
+          BS.concat ([$(TS.lift $ "<" ++ string ++ procAttrs attrs ++ ">")] 
             ++ $(TS.lift nodes)
             ++ [$(TS.lift $ "</" ++ string ++ ">")]
             )
         |]
       where
-        print (Attr name value : attrs) = 
-            " " ++ name ++ "=" ++ value ++ print attrs
-        print [] = ""
+        procAttrs (Attr name value : remainders) = 
+            " " ++ name ++ "=" ++ value ++ procAttrs remainders
+        procAttrs (_ : _) = error "procAttrs: Dash is not allowed"
+        procAttrs [] = ""
     lift (Foreach vals vs nodes) = [|
           BS.concat $ 
             map
@@ -76,29 +75,24 @@ parse = TQ.QuasiQuoter {
 parseLine :: P.Parser (Int, Node)
 parseLine = do
     nextIndent <- parseIndent
-    tag <- valueNode <|> textNode <|> mapNode <|> tagNode
-    P.char '\n'
+    tag <- (valueNode <|> textNode <|> mapNode <|> tagNode) <* P.char '\n'
     return (nextIndent, tag)
   where
-    status indent nextIndent
-        | indent > nextIndent = Child
-        | indent < nextIndent = Parent
-        | otherwise = Sibling
     tagNode = Tag
-        <$> liftM BS.unpack (P.noneOf " \n")
+        <$> BS.unpack <$> (P.noneOf " \n")
         <*> (P.try parseArgs <|> return [])
         <*> return []
       where
         parseArgs = P.token '{' *> (P.sepBy parseArg $ P.token ',') <* P.char '}'
         parseArg = Attr
-            <$> liftM BS.unpack (P.noneOf " :")
-            <*> liftM BS.unpack (P.token ':' *> P.noneOf1 ",}")
+            <$> (BS.unpack <$> (P.noneOf " :"))
+            <*> (BS.unpack <$> (P.token ':' *> P.noneOf1 ",}"))
     mapNode = Foreach
-        <$> liftM BS.unpack (P.string "- foreach " *> P.noneOf " ")
-        <*> liftM (map BS.unpack) (P.string " -> " *> (P.sepBy (P.spaces *> P.noneOf " ,\n") $ P.char ','))
+        <$> (BS.unpack <$> (P.string "- foreach " *> P.noneOf " "))
+        <*> (map BS.unpack <$> (P.string " -> " *> (P.sepBy (P.spaces *> P.noneOf " ,\n") $ P.char ',')))
         <*> return []
-    valueNode = Value <$> liftM BS.unpack (P.string "= " *> P.noneOf "\n")
-    textNode = Text <$> liftM BS.unpack (P.string "| " *> P.noneOf "\n")
+    valueNode = Value <$> BS.unpack <$> (P.string "= " *> P.noneOf "\n")
+    textNode = Text <$> BS.unpack <$> (P.string "| " *> P.noneOf "\n")
 
 parseIndent :: P.Parser Int
 parseIndent = fmap sum $ P.many (
@@ -113,10 +107,10 @@ buildTree ((indent, node) : rest)
     | otherwise  = (indent, (node) : res, remaining)
   where
     (next, res, remaining) = buildTree rest
-    replace (Foreach vals val _) res = Foreach vals val res
-    replace (Tag name attr _) res    = Tag name attr res
-    replace (Text _) res = error "indentation error"
-    replace (Value _) res = error "indentation error"
+    replace (Foreach vals val _) = Foreach vals val
+    replace (Tag name attr _) = Tag name attr
+    replace (Text _) = error "indentation error"
+    replace (Value _) = error "indentation error"
 buildTree []  = 
     (0, [], [])
 
