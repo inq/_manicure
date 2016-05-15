f6435ecaf80ec56d45bc76d7c831581c2f5ef687
inkyu <gofiri@gmail.com>
2016-05-16 08:20:51 +0900

    reduce indentation width in the spec code

--- spec/Core/HtmlSpec.hs
+++ spec/Core/HtmlSpec.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANUGAGE Templatehaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
 module Core.HtmlSpec where
 
@@ -7,40 +6,37 @@ import qualified Data.ByteString.Char8            as BS
 import Core.Html
 import SpecHelper
 
-
-
 spec :: Spec
 spec = 
-    describe "Core.HtmlSpec" $ 
-        context "Simple Text" $ do
-            it "parses simple tag" $ 
-                [parse|html
-                  div
-                    | Hello
-                  |] `shouldBe` "<html><div>Hello</div></html>"
-            it "parses simple variable" $ 
-                [parse|html
-                  div
-                    = theValue
-                  |] `shouldBe` "<html><div>VALUE</div></html>"
-            it "parses simple tag" $ 
-                [parse|html
-                  div
-                    | Hello
-                  |] `shouldBe` "<html><div>Hello</div></html>"
-            it "processes simple foreach statement" $
-                [parse|html
-                  - foreach lines -> name, title
-                    div
-                      p
-                        = name
-                      p
-                        = title
-                  |] `shouldBe` "<html><div><p>A</p><p>B</p></div></html>"
-          where
-              theValue = "VALUE" :: BS.ByteString
-              lines = [["A", "B"] :: [BS.ByteString]]
-
+  describe "Core.HtmlSpec" $ 
+    context "Simple Text" $ do
+      it "parses simple tag" $ 
+        [parse|html
+          div
+            | Hello
+          |] `shouldBe` "<html><div>Hello</div></html>"
+      it "parses simple variable" $ 
+        [parse|html
+          div
+            = theValue
+          |] `shouldBe` "<html><div>VALUE</div></html>"
+      it "parses simple tag" $ 
+        [parse|html
+          div
+            | Hello
+          |] `shouldBe` "<html><div>Hello</div></html>"
+      it "processes simple foreach statement" $
+        [parse|html
+          - foreach people -> name, title
+            div
+              p
+                = name
+              p
+                = title
+          |] `shouldBe` "<html><div><p>A</p><p>B</p></div></html>"
+     where
+      theValue = "VALUE" :: BS.ByteString
+      people = [["A", "B"] :: [BS.ByteString]]
 
 main :: IO ()
 main = hspec spec
