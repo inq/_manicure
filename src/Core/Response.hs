3c92de83ed3db9c536efaa47d5981f69cac3cdf6
inkyu <gofiri@gmail.com>
2016-05-18 18:04:45 +0900

    pass cookie with redirect

--- src/Core/Response.hs
+++ src/Core/Response.hs
@@ -22,15 +22,16 @@ instance Show (BS.ByteString -> Handler) where
 instance Show Handler where
     show _ = ""
 
+cookieToString :: [BS.ByteString] -> [BS.ByteString]
+cookieToString = map (\x -> BS.concat ["Set-Cookie: ", x, "; path=/\r\n"])
+
 renderContent :: Response -> [BS.ByteString]
 -- ^ Render the content
 renderContent (Response _ _ _cookies _content) =
-    map cookieToString _cookies ++
+    cookieToString _cookies ++
     [ "Content-Length: ", BS.pack $ show $ BS.length _content, "\r\n\r\n"
     , _content
     ]
-  where
-    cookieToString cookie = BS.concat ["Set-Cookie: ", cookie, "; path=/\r\n"]
 
 render :: Response -> BS.ByteString
 -- ^ Render to the ByteString
@@ -38,11 +39,12 @@ render r@(Response _ 200 _ _) =
     BS.concat ("HTTP/1.0 200 OK\r\n" : renderContent r)
 render r@(Response _ 404 _ _) =
     BS.concat ("HTTP/1.0 404 Not Found\r\n" : renderContent r)
-render (Response _ 303 _ url) =
-    BS.concat
-      [ "HTTP/1.0 303 See Other\r\n"
-      , "Location: ", url, "\r\n\r\n"
-      ]
+render (Response _ 303 _cookies url) =
+    BS.concat $
+      [ "HTTP/1.0 303 See Other\r\n" ] ++
+      cookieToString _cookies ++ 
+      [ "Location: ", url, "\r\n\r\n"]
+
 render r@(Response _ _ _ _) =
     BS.concat ("HTTP/1.0 500 Internal Error\r\n" : renderContent r)
 
