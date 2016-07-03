9c0b40dacb7544c5d9521b20f52e19bb107400bb
inkyu <gofiri@gmail.com>
2016-07-04 00:42:22 +0800

    must use CR+LF

--- spec/Core/MarkdownSpec.hs
+++ spec/Core/MarkdownSpec.hs
@@ -9,10 +9,10 @@ spec =
   describe "Core.MarkdownSpec" $ do
     context "Simple Text" $ do
       it "parses simple string" $ 
-        parse "#### Hello\n\n" `shouldBe`
+        parse "#### Hello\r\n\r\n" `shouldBe`
           Just (Markdown [H4 "Hello"])
       it "parses simple enippet" $
-        parse "Hello\n```rust\nHELLO\n```\n" `shouldBe`
+        parse "Hello\r\n```rust\r\nHELLO\r\n```\r\n" `shouldBe`
           Just (Markdown [Paragraph "Hello", Snippet "rust" ["HELLO"]])
     context "To Html" $
       it "generate h4 string" $ 
